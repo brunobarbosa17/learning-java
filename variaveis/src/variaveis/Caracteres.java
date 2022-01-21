fdb74610f587c68f0fd9646e42a65bfb1bcc3918
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-01-21 11:20:02 -0300

    ..>

---
+++ variaveis/src/variaveis/Caracteres.java
@@ -0,0 +1,8 @@
+package variaveis;
+
+public class Caracteres {
+	public static void main(String[] args) {
+		String letra = "ooioi";
+		System.out.println(letra);
+	}
+}
---
+++ variaveis/src/variaveis/Variaveis.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		System.out.println("novo teste");
+
+		int idade = 22 - 2;
+		System.out.println("Eu tenho " + idade +" anos");
+		
+		double salario = 6.120;
+		System.out.println(salario);
+		
+	}
+}
