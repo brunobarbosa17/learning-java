75a0208e3d69b9adf1e953972057713022998625
BrunoBarbosa <brunnobarbosas@gmail.com>
2022-04-16 13:19:57 -0300

    ...

--- alura/spring-jpa/src/main/java/br/com/alura/springdata/SpringJpaApplication.java
+++ alura/spring-jpa/src/main/java/br/com/alura/springdata/SpringJpaApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaApplication {
+public class SpringJpaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJpaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
---
+++ alura/spring-jpa/src/main/java/br/com/alura/springdata/model/Cargo.java
@@ -0,0 +1,4 @@
+package br.com.alura.springdata.model;
+
+public class Cargo {
+}
