7c361c324d4a178c001ccf46cfdef817a8243077
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2022-06-05 23:11:41 -0300

    security e autenticacao

--- alura/Spring/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ alura/Spring/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,30 +8,38 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private AutenticacaoService autenticacaoService;
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     // configuracoes de autenticacao
-
+    auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     // configuracoes de autorizacao
     http.authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/topicos")
-            .permitAll()
-            .antMatchers(HttpMethod.GET, "/topicos/*")
-            .permitAll();
+            .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+            .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin();
   }
 
   @Override
   public void configure(WebSecurity web) throws Exception {
     // configuracao de recursos estaticos(js, css, imgs)
 
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new BCryptPasswordEncoder().encode("123456"));
   }
 }
