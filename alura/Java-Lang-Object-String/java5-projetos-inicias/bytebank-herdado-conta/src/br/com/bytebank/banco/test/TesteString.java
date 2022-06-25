9329837a6f7411f90d0a91c543888d3a9cee7920
BrunoBarbosa <brunnobarbosas@gmail.com>
2022-06-25 01:53:09 -0300

    ...

---
+++ alura/Java-Lang-Object-String/java5-projetos-inicias/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,15 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+  public static void main(String[] args) {
+    
+    String nome = "alura";
+    
+    // char c = nome.charAt(0);
+    // System.out.println(nome.indexOf("lura"));
+    // String subnome = nome.substring(2);
+    // System.out.println(subnome);
+
+
+  }    
+}
